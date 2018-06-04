fceaba10784f35a382b6b8482ec1d251dadec980
William <williamliang82@yahoo.com>
2018-06-05 00:06:34 +0700

    add footer links

---
+++ js/footer.js
@@ -0,0 +1,11 @@
+$("#footer-fb").hover(function () {
+  $(this).attr('src', './images/fb-hover.png');
+}, function () {
+  $(this).attr('src', './images/fb.png');
+});
+
+$("#footer-instagram").hover(function () {
+  $(this).attr('src', './images/instagram-hover.png');
+}, function () {
+  $(this).attr('src', './images/instagram.png');
+}); 
